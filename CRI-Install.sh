a9d1271e5952d7f729b6826b463226bfc62a508c
NzSN <aydenlin@foxmail.com>
2022-07-16 03:17:11 +0800

    CRI-Install.sh fix and add cfgs

--- CRI-Install.sh
+++ CRI-Install.sh
@@ -12,23 +12,33 @@ fi
 cd $BUILD_DIR
 
 # Install Containerd
-wget https://github.com/containerd/containerd/releases/download/v1.6.6/containerd-1.6.6-linux-amd64.tar.gz
+if [ ! -f "containerd-1.6.6-linux-amd64.tar.gz" ]; then
+    wget https://github.com/containerd/containerd/releases/download/v1.6.6/containerd-1.6.6-linux-amd64.tar.gz
+fi
 tar Cxzvf /usr/local containerd-1.6.6-linux-amd64.tar.gz
-cp ../containerd.service /etc/systemd/system/
+cp ${WORKPATH}/cfgs/containerd.service /etc/systemd/system/
 
 # Install Runc
-wget https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
+if [ ! -f "runc.amd64" ]; then
+    wget https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
+fi
 install -m 755 runc.amd64 /usr/local/sbin/runc
 
 # Install CNI Plugins
-wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+if [ ! -f "cni-plugins-linux-amd64-v1.1.1.tgz" ]; then
+    wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+fi
+if [ ! -d "/opt/cni/bin" ]; then
+    mkdir -p /opt/cni/bin
+fi
 tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
 
 # Start containerd
-if [ ! -d '/etc/containerd']; then
+if [ ! -d '/etc/containerd' ]; then
     mkdir /etc/containerd
 fi
-containerd config default > /etc/containerd/config.toml
+
+cp ${WORKPATH}/cfgs/config.toml /etc/containerd/config.toml
 
 systemctl daemon-reload
 systemctl enable --now containerd
