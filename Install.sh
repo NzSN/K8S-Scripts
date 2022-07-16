41ec9b770380a8646684227eada07ec70f4fbe00
NzSN <aydenlin@foxmail.com>
2022-07-16 11:46:47 +0800

    Download flanneld

--- Install.sh
+++ Install.sh
@@ -12,3 +12,9 @@ $BASH ./CRI-Install.sh
 
 # Install Kubeadm
 $BASH ./Kubeadm-Install.sh
+
+# Install CNI-Plugins
+mkdir -p /opt/bin
+if [ ! -f "/opt/bin/flanneld" ]; then
+    wget https://github.com/flannel-io/flannel/releases/download/v0.18.1/flanneld-amd64 -O /opt/bin/flanneld
+fi
