f15d0567d436791504035e707ad8cadef100607e
Ubuntu <ubuntu@ip-172-31-4-193.cn-northwest-1.compute.internal>
2022-07-18 07:05:02 +0000

    Kubeadm_install.sh use aliyun

--- Kubeadm-Install.sh
+++ Kubeadm-Install.sh
@@ -7,10 +7,10 @@ apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
 
 # Download the Google Cloud public signing key
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -s https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # Add K8s apt repository
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://mirrors.aliyun.com/kubernetes/apt kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 # Install
 apt-get update
@@ -18,4 +18,19 @@ apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
 # Install Kubernetes Images
-kubeadm config images pull
+images=(
+    kube-apiserver:v1.24.3
+    kube-controller-manager:v1.24.3
+    kube-scheduler:v1.24.3
+    kube-proxy:v1.24.3
+    pause:3.7
+    etcd:3.5.3-0
+)
+
+ALIYUN_ADDR=registry.cn-hangzhou.aliyuncs.com/google_containers
+
+for image in ${images[@]}; do
+    docker pull ${ALIYUN_ADDR}/$image
+done
+
+docker pull coredns/coredns:1.8.6
